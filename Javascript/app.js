460dc53fe017158dbcdd4f36bbafb15ddb3833e9
AA961 <ammar.ahmed961@gmail.com>
2022-05-08 18:40:38 +0500

    pushing

---
+++ Javascript/app.js
@@ -0,0 +1,44 @@
+let days = document.getElementById('days')
+let hours = document.getElementById('hours')
+let minutes = document.getElementById('minutes')
+let seconds = document.getElementById('seconds')
+let dateNow = document.querySelector('.create-date')
+
+const newYearDate = new Date("May 27 , 2022 00:00:00").getTime();
+updateCountDown();
+displayDate();
+
+var currentdate = new Date(); 
+var datetime =  + currentdate.getDate() + " "
+                + (currentdate.getMonth()+1)  + " " 
+                + currentdate.getFullYear()   
+                // + currentdate.getHours() + ":"  
+                // + currentdate.getMinutes() + ":" 
+                // + currentdate.getSeconds();
+
+function displayDate(){
+
+    dateNow.innerHTML = datetime;
+    setTimeout(displayDate,1000)
+
+}
+function updateCountDown(){
+    const now = new Date().getTime();
+    const remaining = newYearDate - now;
+    const totalSeconds = 1000;
+    const totalMinutes = totalSeconds * 60;
+    const totalHours = totalMinutes * 60;
+    const totalDays = totalHours * 24;
+
+    const remainingDays = Math.floor(remaining / totalDays);   
+    const remainingHours = Math.floor((remaining % totalDays) / totalHours);
+    const remainingMinutes = Math.floor((remaining % totalHours) / totalMinutes);
+    const remainingSeconds = Math.floor((remaining % totalMinutes) / totalSeconds);
+
+    days.innerText = remainingDays;
+    hours.innerText = remainingHours;
+    minutes.innerText = remainingMinutes;
+    seconds.innerText = remainingSeconds;
+
+    setTimeout(updateCountDown,1000)
+}
